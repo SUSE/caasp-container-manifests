6cb687c7fcb84eb3bccca187ddf9bae12f2fce07
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2017-03-23 12:57:26 +0100

    redirect errors to standard error
    so that when it is being called from YaST, the errors are rendered

--- activate.sh
+++ activate.sh
@@ -2,21 +2,21 @@
 kube_dir=/etc/kubernetes/manifests
 manifest_dir=/usr/share/caasp-container-manifests
 if [ ! -d $kube_dir ]; then
-    echo "$kube_dir does not exist"
-    echo "make sure kubernetes is installed"
+    echo "$kube_dir does not exist" >&2
+    echo "make sure kubernetes is installed" >&2
     exit -1
 fi
 if [ ! -d $manifest_dir ]; then
-    echo "$manifest_dir does not exist"
-    echo "manifest files are expected to be there"
+    echo "$manifest_dir does not exist" >&2
+    echo "manifest files are expected to be there" >&2
     exit -2
 fi
 if [ ! -f $manifest_dir/salt.yaml ]; then
-    echo "salt.yaml is not in $manifest_dir"
+    echo "salt.yaml is not in $manifest_dir" >&2
     exit -3
 fi
 if [ ! -f $manifest_dir/velum.yaml ]; then
-    echo "velum.yaml is not in $manifest_dir"
+    echo "velum.yaml is not in $manifest_dir" >&2
     exit -3
 fi
 cp -v $manifest_dir/salt.yaml $kube_dir
