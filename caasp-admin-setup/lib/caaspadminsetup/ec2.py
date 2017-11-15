3a0a4034fbf6ac707aafbec03d3d5553104d6370
Joachim Gleissner <jgleissner@suse.com>
2017-11-15 19:28:58 +0100

    CaaSP: handle security group and subnet in setup script
    Look up subnet of admin host and use as subnet default
    Create security group in host's VPC as default security group

--- caasp-admin-setup/lib/caaspadminsetup/ec2.py
+++ caasp-admin-setup/lib/caaspadminsetup/ec2.py
@@ -1,5 +1,10 @@
 import boto3
 import ec2metadata
+import netifaces
+import urllib
+
+_meta_data_base_url = "http://169.254.169.254/2016-09-02/meta-data/"
+_cluster_security_group_id = None
 
 def _get_from_metadata(key):
     return ec2metadata.EC2Metadata().get(key)
@@ -7,11 +12,33 @@ def _get_from_metadata(key):
 def _get_instance_region():
     return _get_from_metadata('availability-zone')[:-1]
 
+def _get_instance_mac():
+    return netifaces.ifaddresses("eth0")[netifaces.AF_LINK][0]["addr"]
+
+def _get_instance_vpc_id():
+    url = "{}/network/interfaces/macs/{}/vpc-id".format(
+                                                     _meta_data_base_url,
+                                                     _get_instance_mac()
+                                                 )
+    val = urllib.urlopen(url).read()
+    return val
+
 def get_local_ipv4():
     return _get_from_metadata('local-ipv4')
 
 def get_instance_id():
     return _get_from_metadata('instance-id')
+
+def get_instance_subnet_id():
+    url = "{}/network/interfaces/macs/{}/subnet-id".format(
+                                                        _meta_data_base_url,
+                                                        _get_instance_mac()
+                                                    )
+    val = urllib.urlopen(url).read()
+    return val
+
+def get_cluster_security_group_id():
+    return _cluster_security_group_id
 
 def create_public_key(key_name, public_key_data):
     client = boto3.client(service_name='ec2', region_name=_get_instance_region())
@@ -20,13 +47,39 @@ def create_public_key(key_name, public_key_data):
         PublicKeyMaterial = public_key_data
     )
 
-def get_salt_cloud_profile_config(image, root_volume_size):
+def create_security_group(security_group_name):
+    global _cluster_security_group_id
+    client = boto3.client(service_name='ec2', region_name=_get_instance_region())
+    response = client.create_security_group(
+                   Description="Autocreated by SUSE CaaSP",
+                   GroupName=security_group_name,
+                   VpcId=_get_instance_vpc_id()
+               )
+    group_id = response["GroupId"]
+    res = boto3.resource(service_name='ec2', region_name=_get_instance_region())
+    sec_group = res.SecurityGroup(group_id)
+    sec_group.authorize_ingress(
+        IpPermissions=
+        [{
+            "IpProtocol": "-1",
+            "UserIdGroupPairs":
+            [{
+                'GroupId': sec_group.group_id
+            }]
+        }]
+    )
+    sec_group.authorize_ingress(
+        IpProtocol="-1",
+        CidrIp="{}/32".format(get_local_ipv4())
+    )
+    _cluster_security_group_id = sec_group.group_id
+
+def get_salt_cloud_profile_config(profile_name, image, root_volume_size):
     config = {
         "worker":  {
             "provider": "ec2",
             "image": image,
-            "size": "SIZE",
-            "script": "/etc/salt/cloud-configure-minion.sh",
+            "script": "/usr/share/caasp-cloud-config/cloud-configure-minion.sh",
             "block_device_mappings": [{
                 "DeviceName": "/dev/sda1",
                 "Ebs.VolumeSize": root_volume_size
@@ -34,8 +87,8 @@ def get_salt_cloud_profile_config(image, root_volume_size):
             "network_interfaces": [{
                 "DeviceIndex": 0,
                 "AssociatePublicIpAddress": False,
-                "SubnetId": "SUBNETID",
-                "SecurityGroupId": "SECURITYGROUPID",
+                "SecurityGroupId": _cluster_security_group_id,
+                "SubnetId": get_instance_subnet_id()
             }]
         }
     }
@@ -51,7 +104,7 @@ def get_salt_cloud_provider_config(key_name, private_key_file):
             "private_key": private_key_file,
             "id": "use-instance-role-credentials",
             "key": "use-instance-role-credentials",
-            "location": "LOCATION",
+            "location": _get_instance_region(),
             "minion": {
                 "master": get_local_ipv4()
             }
