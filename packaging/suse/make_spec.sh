8ebc884a36361e37645476143500f151f59a0a6c
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2018-05-29 11:43:43 +0200

    Revert "Remove image RPM requirements"
    This reverts commit 433738bf4bda5eabce933039b504b5b5690f03c5.
    
    Image RPMs are still required by versions < SLE15

--- packaging/suse/make_spec.sh
+++ packaging/suse/make_spec.sh
@@ -64,6 +64,22 @@ Source:         ${SAFE_BRANCH}.tar.gz
 Requires:       container-feeder
 %endif
 
+# Require all the docker images
+Requires:       %{_base_image}-pause-image >= 2.0.0
+Requires:       %{_base_image}-mariadb-image >= 2.0.0
+Requires:       %{_base_image}-pv-recycler-node-image >= 2.0.0
+Requires:       %{_base_image}-salt-api-image >= 2.0.0
+Requires:       %{_base_image}-salt-master-image >= 2.0.0
+Requires:       %{_base_image}-salt-minion-image >= 2.0.0
+Requires:       %{_base_image}-velum-image >= 2.0.0
+Requires:       %{_base_image}-haproxy-image >= 2.0.0
+Requires:       %{_base_image}-flannel-image >= 2.0.0
+Requires:       %{_base_image}-dnsmasq-nanny-image >= 2.0.0
+Requires:       %{_base_image}-kubedns-image >= 2.0.0
+Requires:       %{_base_image}-sidecar-image >= 2.0.0
+Requires:       %{_base_image}-tiller-image >= 2.0.0
+Requires:       %{_base_image}-openldap-image >= 2.0.0
+Requires:       %{_base_image}-caasp-dex-image >= 2.0.0
 # Require all  the things we mount from the host from the kubernetes-salt package
 Requires:       kubernetes-salt
 BuildArch:      noarch
