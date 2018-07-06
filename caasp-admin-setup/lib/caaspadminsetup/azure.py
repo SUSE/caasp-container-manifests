e04d605398ab7325b4317228e58c5ad04fd085b5
Joachim Gleissner <jgleissner@suse.com>
2018-07-06 18:34:03 +0200

    caasp-admin-setup: account for user set cluster node image
    Use user defined cluster node image if set (in Azure).

--- caasp-admin-setup/lib/caaspadminsetup/azure.py
+++ caasp-admin-setup/lib/caaspadminsetup/azure.py
@@ -19,13 +19,16 @@ def _generate_password():
 
 
 def _get_cluster_node_image_id():
-    # construct URN from procurement flavor and OS release; image IDs from
-    # pint currently do not work in ARM
-    offer = "SUSE-CaaSP-Cluster"
-    if utils.get_from_config("procurement_flavor") == "byos":
-        offer += "-BYOS"
-    sku = utils.get_caasp_release_version()
-    image_to_use = "SUSE|{}|{}|latest".format(offer, sku)
+    # use user set cluster node image if any
+    image_to_use = utils.get_from_config("cluster_image")
+    if not image_to_use:
+        # construct URN from procurement flavor and OS release; image IDs from
+        # pint currently do not work in ARM
+        offer = "SUSE-CaaSP-Cluster"
+        if utils.get_from_config("procurement_flavor") == "byos":
+            offer += "-BYOS"
+        sku = utils.get_caasp_release_version()
+        image_to_use = "SUSE|{}|{}|latest".format(offer, sku)
     logging.info('Using cluster node image with name: "%s"' % image_to_use)
     return image_to_use
 
