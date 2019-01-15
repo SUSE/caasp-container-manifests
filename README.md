ca48c0c02050916aa9ebca2baec854db69bfac6f
Joachim Gleissner <jgleissner@suse.com>
2019-01-15 16:10:13 +0100

    Moved cloud related files into new directory

--- README.md
+++
@@ -1,1 +0,0 @@
-Sources we will add to CaaSP for use in the public cloud.
--- setup_caas_node.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "master: IP_ADDRESS_OF_ADMIN_NODE" > /etc/salt/minion.d/master.conf
-systemctl enable salt-minion
-systemctl start salt-minion
